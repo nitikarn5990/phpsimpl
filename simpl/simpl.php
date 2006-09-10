35930716767a94d9bfd7e252acc52a21fa4dd617
nick.denardis <nick.denardis@dfa5d3ea-9a19-0410-bf0f-4f84e9755593>
2006-09-10 14:58:56 +0000

    New Error functions require a session so the default simpl loader now makes sure there is a session started.

--- simpl/simpl.php
+++ simpl/simpl.php
@@ -1,1 +1,29 @@
-<?php	// Include the Config	if (defined(FS_SIMPL))		include_once(FS_SIMPL . 'config.php');	else		include_once(DIR_ABS . 'simpl/config.php');		// Include the functions	include_once(FS_SIMPL . 'functions.php');		// Include the Simpl Loader class	include_once(FS_SIMPL . 'main.php');		// Load the Base Classes	$mySimpl = new Simpl;		// Clear Cache if need be	if (CLEAR_CACHE === true){		$current_cache = glob(FS_SIMPL . WS_CACHE . '*.cache');		if (is_array($current_cache))			foreach ($current_cache as $filename)				unlink($filename);		unset($current_cache);	}?>
+<?php
+	// Start a session if not already started
+	if (session_id() == '')
+		session_start();
+
+	// Include the Config
+	if (defined(FS_SIMPL))
+		include_once(FS_SIMPL . 'config.php');
+	else
+		include_once(DIR_ABS . 'simpl/config.php');
+	
+	// Include the functions
+	include_once(FS_SIMPL . 'functions.php');
+	
+	// Include the Simpl Loader class
+	include_once(FS_SIMPL . 'main.php');
+	
+	// Load the Base Classes
+	$mySimpl = new Simpl;
+	
+	// Clear Cache if need be
+	if (CLEAR_CACHE === true){
+		$current_cache = glob(FS_SIMPL . WS_CACHE . '*.cache');
+		if (is_array($current_cache))
+			foreach ($current_cache as $filename)
+				unlink($filename);
+		unset($current_cache);
+	}
+?>
