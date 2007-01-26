c3321ef64603e9d6132a8dc9afc491247a59c429
nick.denardis <nick.denardis@dfa5d3ea-9a19-0410-bf0f-4f84e9755593>
2007-01-26 10:38:38 +0000

    Added some more Config values and cleaned up the order so FS_CACHE was always defined and the classes would not have to keep checking if it were defined or not.

--- simpl/config.php
+++ simpl/config.php
@@ -1,17 +1,21 @@
 <?php
 // Always Include trailing slash "/" in Direcories
 define('DEBUG',false);
+define('DEBUG_QUERY',false);
 define('USE_CACHE',true);
+define('QUERY_CACHE',true);
 define('CLEAR_CACHE',false);
 
 // Where things are sitting
+define('DIR_ABS','./');
 define('WS_SIMPL','simpl/');
-define('FS_SIMPL',DIR_ABS . WS_SIMPL);
 define('WS_SIMPL_IMAGE','img/');
 define('WS_SIMPL_INC','inc/');
 define('WS_SIMPL_CSS','css/');
 define('WS_SIMPL_JS','js/');
 define('WS_CACHE','cache/');
+define('FS_SIMPL',DIR_ABS . WS_SIMPL);
+define('FS_CACHE',FS_SIMPL . WS_CACHE);
 
 // Database Connection Option
 define('DB_USER',DBUSER);
