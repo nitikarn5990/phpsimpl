540d0946952eb05bbdaf559042b19f5c603afedc
nick.denardis <nick.denardis@dfa5d3ea-9a19-0410-bf0f-4f84e9755593>
2007-12-04 22:41:44 +0000

    Added the ID to the hidden fields.

--- trunk/simpl/field.php
+++ trunk/simpl/field.php
@@ -198,7 +198,7 @@ class Field {
 			return true;
 		}else if ($this->Get('display') == 0){
 			// Hidden
-			echo '<input name="' . $name . (($multi)?'[]':'') . '" type="hidden" value="' . urlencode($this->Output($my_value)) . '" />' . "\n";
+			echo '<input name="' . $name . (($multi)?'[]':'') . '" type="hidden" id="' . $this->Get('name') . (($multi)?'_' . $this->Get('multi'):'') . '" value="' . urlencode($this->Output($my_value)) . '" />' . "\n";
 			return true;
 		}
 		
