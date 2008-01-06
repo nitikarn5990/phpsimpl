0aa6dfdb0a97c957ee5ffc8d8150a221c4553074
nick.denardis <nick.denardis@dfa5d3ea-9a19-0410-bf0f-4f84e9755593>
2008-01-06 14:00:55 +0000

    Removed the username and password form the example define.

--- trunk/examples/inc/define.php
+++ trunk/examples/inc/define.php
@@ -45,8 +45,8 @@ define('FS_CACHE', DIR_ABS . 'cache/');
 
 
 // Database Connection Options
-define('DB_USER', 'nick');
-define('DB_PASS', 'trysk8ting');
-define('DB_HOST', 'localhost');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_HOST', '');
 define('DB_DEFAULT', 'simpl_example');
 ?>
