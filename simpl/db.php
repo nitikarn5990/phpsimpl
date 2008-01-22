663b9e75a2d9bf429b5101cc58812e4ce5558802
nick.denardis <nick.denardis@dfa5d3ea-9a19-0410-bf0f-4f84e9755593>
2008-01-22 19:52:26 +0000

    Rolled back the Lazy DB change. It needs to be re-evaluated before rolled in since not all applications pass the DB with each query.

--- simpl/db.php
+++ simpl/db.php
@@ -155,6 +155,10 @@ class DB {
     	
     	// Increment the query counter
     	$this->query_count++;
+    	
+    	// Change the DB back is needed
+    	if ($db != '' && $db != $this->database)
+    		$this->Change($old_db);
     		
     	// Cache the Query if possible
     	if ($is_cache){
