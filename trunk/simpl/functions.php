2521b10b0397f9f369c2598f73c62774ce9da7b7
nick.denardis <nick.denardis@dfa5d3ea-9a19-0410-bf0f-4f84e9755593>
2008-03-09 01:06:19 +0000

    Added the DateTimeDiff() function to the global functions to produce a human readable "time ago" output. So passing a specific date and time to the function will produce "13 seconds ago" or "2 hour ago"

--- trunk/simpl/functions.php
+++ trunk/simpl/functions.php
@@ -152,6 +152,78 @@ if (!function_exists('Debug')){
 	}
 }
 
+/**
+ * Displays time difference in readable way
+ *
+ * @param $data_ref date to compare against in 0000:00:00 00:00:00 format
+ * @return string
+ */
+if (!function_exists('DateTimeDiff')){
+function DateTimeDiff($data_ref){
+	$current_date = date('Y-m-d H:i:s');
+
+	// Extract $current_date
+	$current_year = substr($current_date,0,4);	
+	$current_month = substr($current_date,5,2);
+	$current_day = substr($current_date,8,2);
+
+	// Extract from $data_ref
+	$ref_year = substr($data_ref,0,4);
+	$ref_month = substr($data_ref,5,2);
+	$ref_day = substr($data_ref,8,2);
+
+	// create a string yyyymmdd 20071021
+	$tempMaxDate = $current_year . $current_month . $current_day;
+	$tempDataRef = $ref_year . $ref_month . $ref_day;
+
+	$tempDifference = $tempMaxDate-$tempDataRef;
+
+	// If the difference is GT 10 days show the date
+	if($tempDifference >= 10){
+		$str = 'F j, Y \\a\\t g:i a';
+		if (date('Y', strtotime($data_ref)) == date('Y'))
+			$str = 'F j \\a\\t g:i a';
+		
+		return date($str, strtotime($data_ref));
+	}else{
+		// Extract $current_date H:m:ss
+		$current_hour = substr($current_date,11,2);
+		$current_min = substr($current_date,14,2);
+		$current_seconds = substr($current_date,17,2);
+
+		// Extract $data_ref Date H:m:ss
+		$ref_hour = substr($data_ref,11,2);
+		$ref_min = substr($data_ref,14,2);
+		$ref_seconds = substr($data_ref,17,2);
+	
+		$hDf = $current_hour-$ref_hour;
+		$mDf = $current_min-$ref_min;
+		$sDf = $current_seconds-$ref_seconds;
+	
+		// Show time difference ex: 2 min 54 sec ago.
+		if($dDf<1){
+			if($hDf>0){
+				if($mDf<0){
+					$mDf = 60 + $mDf;
+					$hDf = $hDf - 1;
+					return $mDf . ' min ago';
+				} else {
+					return $hDf. ' hr ' . $mDf . ' min ago';
+				}
+			} else {
+				if($mDf>0){
+					return $mDf . ' min ' . $sDf . ' sec ago';
+				} else {
+					return $sDf . ' sec ago';
+				}
+			}
+		} else {
+			return $dDf . ' days ago';
+		}
+	}
+}
+}
+
 function search_split_terms($terms){
 	$terms = preg_replace("/\"(.*?)\"/e", "search_transform_term('\$1')", $terms);
 	$terms = preg_split("/\s+|,/", $terms);
