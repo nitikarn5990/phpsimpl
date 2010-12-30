d2eadd8dd06eb1be24061b78f28122eb6e61d61b
nick.denardis <nick.denardis@dfa5d3ea-9a19-0410-bf0f-4f84e9755593>
2010-12-30 19:52:58 +0000

    Started to build the relationships for ActiveRecords. Still a little up in the air. HasMany seems to be functioning with Count() and Items(), but still a TON of work needs to be done. Not production ready.

--- trunk/simpl/activerecord.php
+++ trunk/simpl/activerecord.php
@@ -23,6 +23,10 @@ class ActiveRecord extends DbTemplate {
 	 * @var array
 	 */
 	private $has_and_belongs_to_many = array();
+	/**
+	 * @var array
+	 */
+	private $associations = array();
 	
 	/**
 	 * Active Record Constructor
@@ -35,12 +39,90 @@ class ActiveRecord extends DbTemplate {
 		parent::__construct($table, $database);
 	}
 	
+	public function __call($method, $args){
+		// Make sure the class called is valid
+		if (!class_exists($method))
+			echo 'No Class Exists';
+		
+		// Make sure the class called has an association
+		if (!isset($this->associations[$method]))
+			echo 'No Association Exists';
+		
+		// Require there is a primary key set
+		if ($this->GetPrimary() == '')
+			die('No primary key set');
+		
+		// Get the method these two classes are joined
+		$join_type = $this->associations[$method];
+		
+		// Localize the config info
+		$join_info = $this->$join_type;
+		
+		// See if there is a through class
+		$through = $join_info[$method]['through'];
+		
+		// Create the relationship object to be returned
+		$myObject = new $method;
+		
+		// Set the primary key if there is one
+		$myObject->SetValue($this->primary, $this->GetPrimary());
+		
+		// Join the classes with the through
+		if ($through != ''){
+			$myThrough = new $through;
+			$myThrough->SetValue($this->primary, $this->GetPrimary());
+			$this->Join($myThrough, $this->primary, 'LEFT');
+		}
+		
+		// Join the relationship object with the through object
+		$myObject->Join($myThrough, $myObject->primary, 'LEFT');
+
+		// Return the compiled Object
+		return $myObject;
+	}
+	
+	/**
+	 * Count
+	 * Gets the number of elements related to the relationship
+	 * 
+	 * @return int Items in the relationship
+	 */
+	public function Count(){
+		// Make sure there is a primary key set
+		// @todo Not sure how to do this from here
+				
+		// Join all the tables and determine the count
+		$this->GetList('count');
+
+		// Return the count		
+		return $this->results['count'];
+	}
+	
+	/**
+	 * Items
+	 * Gets an array of the relationship items
+	 * 
+	 * @param array $display Fields to be returned
+	 * @return array Of relationship items
+	 */
+	public function Items($display=''){
+		// Make sure there is a primary key set
+		// @todo Not sure how to do this from here
+				
+		// Join all the tables and determine the count
+		$this->GetList($display);
+		
+		// Return the count		
+		return $this->Results();
+	}
+
+	
 	/**
 	 * Belongs To
 	 * Foreign Key in $class table
 	 * 
 	 * @param object $class ActiveRecord
-	 * @return bool
+	 * @return int Items in the array
 	 */
 	public function BelongsTo($class){
 		return array_push($this->belongs_to, $class);
@@ -51,7 +133,7 @@ class ActiveRecord extends DbTemplate {
 	 * Foreign Key in $class table
 	 * 
 	 * @param object $class ActiveRecord
-	 * @return bool
+	 * @return int Items in the array
 	 */
     public function HasOne($class){
 		return array_push($this->has_one, $class);
@@ -62,10 +144,12 @@ class ActiveRecord extends DbTemplate {
 	 * Foreign Key in $class table
 	 * 
 	 * @param object $class ActiveRecord
-	 * @return bool
+	 * @return int Items in the array
 	 */
-    public function HasMany($class){
-		return array_push($this->has_many, $class);
+    public function HasMany($class, $through=''){
+    	$this->associations[$class] = 'has_many';
+		$this->has_many[$class] = array('class' => $class, 'through' => $through);
+		return true;
     }
     
      /**
@@ -73,12 +157,17 @@ class ActiveRecord extends DbTemplate {
 	 * Foreign Key in $class table
 	 * 
 	 * @param object $class ActiveRecord
-	 * @return bool
+	 * @return int Items in the array
 	 */
     public function HasAndBelongsToMany($class){
 		return array_push($this->has_and_belongs_to_many, $class);
     }
 	
+	/**
+	 * Active Record Destructor
+	 * 
+	 * @return NULL
+	 */
 	public function __destruct() {
 		// Get a list of all the class variables
 		unset($this);
